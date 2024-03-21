ab464b867ed1b80ef6fadcda7ffcaf5f61bba120
einarno <86827863+einarno@users.noreply.github.com>
2024-03-21 07:43:04 +0100

    Add components

---
+++ apps/crud/src/containers/commits/index.ts
@@ -0,0 +1,18 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+type Props = {
+
+}
+export const CommitsTable = () => {
+  return (
+
+  )
+}
